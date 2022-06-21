6d12d82d079ad580885c06412f690028d11553c6
Legrain Samuel <samuel.legrain@bstorm.be>
2022-06-21 14:52:48 +0200

    Add Exo IBAN

---
+++ Exo-IBAN/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exo_IBAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez introduire votre BBAN (12 chiffres) :");
+
+            string bban = Console.ReadLine();
+            if (bban.Length == 12 && long.TryParse(bban, out _))
+            {
+                string recup = bban.Substring(10);
+                string code = recup + recup + "111400";
+                long codeTrans = long.Parse(code);
+                short numBe = (short)(98 - (codeTrans % 97));
+                Console.WriteLine($"BE{((numBe<10)?"0"+numBe:numBe)}{bban}");
+            }
+            else
+            {
+                Console.WriteLine($"Votre BBAN '{bban}' n'est pas valide...");
+            }
+        }
+    }
+}
