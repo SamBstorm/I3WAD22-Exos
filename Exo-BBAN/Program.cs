a2cfd173f2379bb998a19855dc2442399ebca7ec
Legrain Samuel <samuel.legrain@bstorm.be>
2022-06-21 13:59:28 +0200

    Add Exos Divisions & BBAN

---
+++ Exo-BBAN/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exo_BBAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour! Veuillez introduire votre numéro BBAN (12 chiffres):");
+            string bban = Console.ReadLine();
+            if (
+                (bban.Length == 12 && long.TryParse(bban, out _)) //SOIT BBAN (12 chiffres)
+                || 
+                (bban.Length == 16 && long.TryParse(bban.Substring(bban.Length-14), out _))) //SOIT IBAN 2 Lettres + 14 chiffres
+            {
+                string twoLast = bban.Substring(bban.Length - 2);
+                if (twoLast == "00" || twoLast == "98" || twoLast == "99") Console.WriteLine("KO"); // Plus facile de prouver que le compte n'est pas valide, si celui-ci se termine par 00, 98 ou 99, alors il n'est pas valide, pas besoin de calcul ou de convertion pour le prouver...
+                else
+                {
+                    string tenFirst = bban.Substring(bban.Length - 12, 10);
+                    long tenNumber = long.Parse(tenFirst);
+                    short twoNumber = short.Parse(twoLast);
+                    short modulo = (short)(tenNumber % 97);
+                    if (twoNumber == modulo || (modulo == 0 && twoNumber == 97)) Console.WriteLine("OK");
+                    else Console.WriteLine("KO");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Votre numéro BBAN '{bban}' ne semble pas valide... ");
+            }
+        }
+    }
+}
---
+++ Exo-Divisions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exo_Divisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez indiquer un permier nombre :");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int a))
+            {
+                Console.WriteLine("Veuillez introduire un second nombre :");
+                input = Console.ReadLine();
+                if (int.TryParse(input, out int b) && b != 0) {
+                    Console.WriteLine($"Division entière : { a / b } \t Modulo : { a % b } \t Division : { (double) a / b }");
+                }
+                else { Console.WriteLine($"Le nombre {input} n'est pas valide..."); }
+            }
+            else { Console.WriteLine($"Le nombre {input} n'est pas valide..."); }
+        }
+    }
+}
