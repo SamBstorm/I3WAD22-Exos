f0e184ef45a9128ebc5be31b7514d3a8971f2da4
Legrain Samuel <samuel.legrain@bstorm.be>
2022-07-05 16:50:08 +0200

    Add Exo "Matrice de point"

---
+++ Exo-MatricePoint/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exo_MatricePoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TAILLE = 5;
+            Point?[,] tab = new Point?[TAILLE, TAILLE];
+
+            for (int i = 0; i < TAILLE; i++)
+            {
+                Point p;
+                p.x = i + 1;
+                p.y = i + 1;
+                tab[i, i] = p;
+            }
+
+            for (int row = 0; row < TAILLE; row++)
+            {
+                for (int col = 0; col < TAILLE; col++)
+                {
+                    if (tab[row, col] is null) Console.Write("\t");
+                    else
+                    {
+                        Point p = (Point)tab[row, col];
+                        Console.Write($"X : {p.x} - Y : {p.y}");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Exo-MatricePoint/Structs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_MatricePoint
+{
+    public struct Point {
+        public int x, y;
+    }
+}
