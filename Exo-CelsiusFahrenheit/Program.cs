ea72a632a17cc423ea1303491bb782a697697944
Legrain Samuel <samuel.legrain@bstorm.be>
2022-07-06 16:12:39 +0200

    Add Exo "Celsius Fahrenheit"

---
+++ Exo-CelsiusFahrenheit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exo_CelsiusFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celsius c;
+            c.temperature = 21;
+            Console.WriteLine($"{c.temperature}°C vaut {c.Conversion().temperature}°F.");
+
+
+            Fahrenheit f;
+            f.temperature = 210;
+            Console.WriteLine($"{f.temperature}°F vaut {f.Conversion().temperature}°C.");
+        }
+    }
+}
---
+++ Exo-CelsiusFahrenheit/Structs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_CelsiusFahrenheit
+{
+    public struct Celsius {
+        public double temperature;
+        public Fahrenheit Conversion()
+        {
+            Fahrenheit f;
+            f.temperature = (this.temperature * 9 / 5) + 32;
+            return f;
+        }
+    }
+
+    public struct Fahrenheit
+    {
+        public double temperature;
+        public Celsius Conversion()
+        {
+            Celsius c;
+            c.temperature = (this.temperature - 32) * 5 / 9;
+            return c;
+        }
+    }
+}
