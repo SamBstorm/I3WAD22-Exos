2d6354da3c2bfbdb173aff11c55bfe551cd26034
Legrain Samuel <samuel.legrain@bstorm.be>
2022-07-07 13:36:47 +0200

    Add Exo "Equation second degré"

---
+++ Exo-EquationSecondDegre/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exo_EquationSecondDegre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation eq1;
+            eq1.a = 1;
+            eq1.b = -2;
+            eq1.c = 1;
+
+            double? X1, X2;
+
+            if(eq1.Resoudre(out X1,out X2))
+            {
+                if (X1 == X2) Console.WriteLine($"{eq1.a}X² + {eq1.b}X + {eq1.c} est résolvable et X vaut {X1}.");
+                else Console.WriteLine($"{eq1.a}X² + {eq1.b}X + {eq1.c} est résolvable et X peut être {X1} ou {X2}.");
+            }
+            else
+            {
+                Console.WriteLine($"{eq1.a}X² + {eq1.b}X + {eq1.c} n'est pas résolvable...");
+            }
+        }
+    }
+}
---
+++ Exo-EquationSecondDegre/Structs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_EquationSecondDegre
+{
+    public struct Equation {
+        public double a, b, c;
+
+        public bool Resoudre(out double? x1, out double? x2)
+        {
+            double delta = (b * b) - (4 * a * c);
+
+            if (delta < 0)
+            {
+                x1 = null;
+                x2 = null;
+                return false;
+            }
+            else if (delta == 0)
+            {
+                x1 = -b / (2 * a);
+                x2 = x1;
+                return true;
+            }
+            else
+            {
+                x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                return true;
+            }
+        }
+
+        public bool ResoudreSimplifie(out double? x1, out double? x2)
+        {
+            double delta = (b * b) - (4 * a * c);
+            x1 = x2 = null;
+            if(delta < 0) return false;
+            if (delta == 0) x1 = x2 = -b / (2 * a);
+            else {
+                x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            }
+            return true;
+        }
+
+        public bool ResoudreFinal(out double? x1, out double? x2)
+        {
+            double delta = (b * b) - (4 * a * c);
+            x1 = x2 = null;
+            if (delta < 0) return false;
+            x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            return true;
+        }
+    }
+}
